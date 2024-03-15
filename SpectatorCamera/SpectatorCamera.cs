cc0782d923c88e26286d3c693d8cb20a13af291f
Doom <doomsicklesgt72@gmail.com>
2024-03-15 14:55:55 -0400

    actually download the spectator camera asset bundle

--- SpectatorCamera/SpectatorCamera.cs
+++ SpectatorCamera/SpectatorCamera.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Collections;
 using System.IO;
+using MelonLoader;
+using MelonLoader.Utils;
 using UnityEngine;
+using UnityEngine.Networking;
 using Object = UnityEngine.Object;
 
 namespace LibOnward.SpectatorCamera;
 
 public static class SPCamera
 {
+    private static readonly object _lock = new object();
+    
+    internal static IEnumerator internal_handleSpectatorCameraDownload(int fov, float smooth)
+    {
+        Logger.At("SpectatorCamera::internal_handleSpectatorCameraDownload");
+        
+        var saveTo = Path.Combine(MelonEnvironment.ModsDirectory, "libonward_assets") + "\\spec_camera";
+
+        lock (_lock)
+        {
+            if (GameObject.Find("Spectator Camera(Clone)") != null)
+            {
+                Logger.Warn("Spectator Camera already exists, skipping creation");
+                yield break;
+            }
+        
+            if (!File.Exists(saveTo))
+            {
+                var url = "dooomsickle.github.io/LibOnward/bundles/spec_camera";
+
+                var req = UnityWebRequest.Get(url);
+        
+                yield return req.SendWebRequest();
+        
+                if (req.isNetworkError || req.isHttpError)
+                    Logger.Except<Exception>($"Failed to download spectator camera bundle: {req.error}");
+        
+                File.WriteAllBytes(saveTo, req.downloadHandler.data);
+            }
+        
+            var myLoadedAssetBundle = AssetBundle.LoadFromFile(saveTo);
+            if (myLoadedAssetBundle == null)
+                Logger.Except<Exception>("Failed to load AssetBundle");
+
+            var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("Spectator Camera.prefab");
+            var go = Object.Instantiate(prefab);
+
+            myLoadedAssetBundle.Unload(false);
+            go.AddComponent<SmoothFollowBehavior>().smooth = smooth;
+            go.GetComponentInChildren<Camera>().cameraType = CameraType.SceneView;
+            go.GetComponentInChildren<Camera>().fieldOfView = fov;
+        }
+    }
+
     public static void Create(int fov = 90, float smooth = 0.5f)
     {
         Logger.At("SpectatorCamera::Create");
@@ -17,18 +65,8 @@ public static class SPCamera
             return;
         }
 
-    Logger.Log("Creating Spectator Camera");
+        Logger.Log("Creating Spectator Camera");
         
-        var myLoadedAssetBundle = AssetBundle.LoadFromFile(@"C:\Users\sgtdo\My project (1)\MyBuild\meshes");
-        if (myLoadedAssetBundle == null)
-            Logger.Except<Exception>("Failed to load AssetBundle");
-
-        var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("Spectator Camera.prefab");
-        var go = Object.Instantiate(prefab);
-
-        myLoadedAssetBundle.Unload(false);
-        go.AddComponent<SmoothFollowBehavior>().smooth = smooth;
-        go.GetComponentInChildren<Camera>().cameraType = CameraType.SceneView;
-        go.GetComponentInChildren<Camera>().fieldOfView = fov;
+        MelonCoroutines.Start(internal_handleSpectatorCameraDownload(fov, smooth));
     }
 }
